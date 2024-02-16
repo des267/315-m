ff33f7032a8bf700be202c938e56ac7e8887c2a9
Desmond Stular <dstular@yahoo.com>
2024-02-16 12:35:24 -0700

    [BE] Added index.js which connects to database and uses routes created for students and courses.

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,27 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import connectDB from "./services/database.service.js";
+import studentRoute from "./routes/student.route.js";
+import courseRoute from "./routes/course.route.js";
+
+const app = express();
+const port = 3000;
+
+// Connect to database
+connectDB();
+
+// Make express use body-parser json
+app.use(bodyParser.json());
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Routes
+app.use("/student", studentRoute);
+app.use("/course", courseRoute);
+
+// Configure listening
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -7,3 +7,15 @@
  * Author: Desmond Stular
  * Date: Feb 16, 2024
  */
+
+import express from "express";
+
+const router = express.Router();
+
+router.get("/");
+router.get("/:id");
+router.post("/");
+router.patch("/:id");
+router.delete("/:id");
+
+export default router;
