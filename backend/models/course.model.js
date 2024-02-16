0c3aa6d2abf1d2302eeb238e9939f4d7f1e6996b
Desmond Stular <dstular@yahoo.com>
2024-02-16 12:11:32 -0700

    [BE] Created models for students and courses.

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -4,3 +4,22 @@
  * Contains the model/mongoose schema for a course
  * in the database.
  */
+
+import mongoose from "mongoose";
+
+const CourseSchema = new mongoose.Schema(
+	{
+		"id": {type: Number, required: true, unique: true},
+		"name": {type: String, required: true},
+		"department": {type: String, required: true},
+		"students": [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Student"
+		}]
+	},
+	{strictQuery: true}
+);
+
+const Course = new mongoose.model("Course", CourseSchema);
+
+export default Course;
--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -5,3 +5,22 @@
  * Author: Desmond Stular
  * Date: Feb 16, 2024
  */
+
+import mongoose from "mongoose";
+
+const StudentSchema = new mongoose.Schema(
+	{
+		"id": {type: Number, required: true, unique: true},
+		"name": {type: String, required: true},
+		"email": String,
+		"courses": [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Course"
+		}]
+		},
+	{strictQuery: true}
+);
+
+const Student = mongoose.model("Student", StudentSchema);
+
+export default Student;
