05ab1e404c39eaab33974e186a3ffb33fb8dbcd0
Desmond Stular <dstular@yahoo.com>
2024-02-16 13:00:47 -0700

    [BE] Created CRUD operations inside student and course repos

--- backend/repos/course.repo.js
+++ backend/repos/course.repo.js
@@ -8,3 +8,42 @@
  * Author: Desmond Stular
  * Date: Feb 16, 2024
  */
+
+import Course from "../models/course.model.js";
+
+export const getCoursesFromRepo = async (query) => {
+	try {
+		return await Course.find(query);
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const updateCourseInRepo = async (query, update) => {
+	try {
+		return await Course.findOneAndUpdate(
+			{...query},
+			{...update},
+			{new: true}
+		).lean()
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const deleteCourseFromRepo = async (query) => {
+	try {
+		return await Course.findOneAndDelete({...query});
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const createCourseInRepo = async (payload) => {
+	try {
+		const newCourse = new Course(payload);
+		return await newCourse.save();
+	} catch (e) {
+		throw e;
+	}
+}
--- backend/repos/student.repo.js
+++ backend/repos/student.repo.js
@@ -8,3 +8,42 @@
  * Author: Desmond Stular
  * Date: Feb 16, 2024
  */
+
+import Student from "../models/student.model.js";
+
+export const getStudentsFromRepo = async (query) => {
+	try {
+		return await Student.find(query);
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const updateStudentInRepo = async (query, update) => {
+	try {
+		return await Student.findOneAndUpdate(
+			{...query},
+			{...update},
+			{new: true}
+		).lean()
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const deleteStudentFromRepo = async (query) => {
+	try {
+		return await Student.findOneAndDelete({...query});
+	} catch (e) {
+		throw e;
+	}
+}
+
+export const createStudentInRepo = async (payload) => {
+	try {
+		const newStudent = new Student(payload);
+		return await newStudent.save();
+	} catch (e) {
+		throw e;
+	}
+}
