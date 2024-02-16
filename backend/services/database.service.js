27e68d161132360b434c83857f469f4e78c6ffd8
Desmond Stular <dstular@yahoo.com>
2024-02-16 12:34:55 -0700

    [BE] Created database connection

--- backend/services/database.service.js
+++ backend/services/database.service.js
@@ -9,4 +9,16 @@
 
 import mongoose from "mongoose";
 
+const localUrl = "localhost:3000/"
 mongoose.set('strictQuery', true);
+
+const connectDB = async () => {
+	try {
+		const connection = await mongoose.connect(process.env.DATABASE_URL || localUrl, {dbName: "315M"});
+		console.log("Connected to database");
+	} catch (e) {
+		console.log("Could not connect to database");
+	}
+};
+
+export default connectDB;
