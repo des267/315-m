42f751843d9249b2d4101703e757dfad5d738bc9
Desmond Stular <dstular@yahoo.com>
2024-02-17 15:46:45 -0700

    [BE] Changed localhost port to 8001 for express.

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,23 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import connectDB from "./services/database.service.js";
 import studentRoute from "./routes/students.route.js";
 import courseRoute from "./routes/courses.route.js";
 import homeRoute from "./routes/home.route.js";
 
 const app = express();
-const port = 3000;
+const port = 8001;
 
 // Connect to database
 connectDB();
 
 // Make express use body-parser json
 app.use(bodyParser.json());
+
+// Enable All CORS requests
+app.use(cors());
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
