acc588dcf8889830d0ac41a5711103b183db0609
Desmond Stular <dstular@yahoo.com>
2024-02-18 15:52:43 -0700

    [BE] Added connection string for render application

--- backend/services/database.service.js
+++ backend/services/database.service.js
@@ -9,12 +9,12 @@
 
 import mongoose from "mongoose";
 
-const localUrl = "mongodb://localhost:27017/315M"
+const localUrl = "mongodb://localhost:27017/"
 mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(localUrl);
+		await mongoose.connect(localUrl || process.env.DATABASE_URL, {dbName: "315m"});
 		console.log("Connected to database");
 	} catch (e) {
 		console.log("Could not connect to database");
